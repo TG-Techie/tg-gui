1c6ab0421315d808ce5532a55bc94777f3dc8b98
Jonah <tgtechie01@gmail.com>
2022-05-30 17:56:50 -0400

    Remove extra import wildcard for platform alias

--- tg_gui/platform/__init__.py
+++ tg_gui/platform/__init__.py
@@ -1,6 +1,6 @@
 # import tg_gui_core before typing for circuitpython compatibility
 # this only needs ot happen once
-import tg_gui_core
+import tg_gui_core as _tg_gui_core_
 
 from typing import TYPE_CHECKING
 
@@ -9,14 +9,14 @@ from sys import implementation as _implementation, modules as _sys_modules
 
 if TYPE_CHECKING or _implementation.name == "cpython":
     from .. import _platform_qt_ as _platform_  # type: ignore
-    from .._platform_qt_ import *
 elif _implementation.name == "circuitpython":
     from .. import _platform_displayio_ as _platform_  # type: ignore
-    from .._platform_displayio_ import *
 else:
     raise NotImplementedError(
         "tg_gui does not have an implementation for this python implementation"
         + f" ({_implementation.name}) and/or platform"
     )
 
+# alias the implementation for this platform to `tg_gui.platform`
 _sys_modules["tg_gui.platform"] = _platform_
+del _implementation, _sys_modules, TYPE_CHECKING  # do not release the _platform_ module
